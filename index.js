e9fc5e9c66bc3d706741b230445486e8508ee798
Deni0012 <deni.dg00@icloud.com>
2025-03-27 15:20:18 +0100

    feat: aggiunta di app use e app get

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5500;
+
+app.use(express.static('public'));
+
+app.get('/', (req, resp) => {
+    rend.send("Server del mio blog");
+})
