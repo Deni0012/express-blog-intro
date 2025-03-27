c672b9088c6e54fe1b1f573a6c4f876ea3e96620
Deni0012 <deni.dg00@icloud.com>
2025-03-27 15:42:26 +0100

    feat: creazione di post.js e aggiunto a index.js tramite require

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const post = require("/post.js");
 const app = express();
 const port = 5500;
 
 app.use(express.static('public'));
 
+//Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
 app.get('/', (req, resp) => {
     rend.send("Server del mio blog");
 })
---
+++ post.js
@@ -0,0 +1,41 @@
+//Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags(tags è un array di stringhe)
+post = [
+    {
+        title: "Ciambellone",
+        text: "Uova, farina, lievito, latte...",
+        img: "/public/images/ciambellone.jpeg",
+        tags: ["1", "2", "3"]
+
+    },
+    {
+        title: "Cracker Barbabietola",
+        text: "Cereali, Farina, Acqua, Barbabietola",
+        img: "/public/images/cracker_barbabietola.jpeg",
+        tags: ["1", "2", "3"]
+
+    }, 
+    {
+        title: "Pane Fritto Dolce",
+        text: "Acqua, farina, lievito, olio...",
+        img: "/public/images/pane_fritto_dolce.jpeg",
+        tags: ["1", "2", "3"]
+
+    },
+    {
+        title: "Pasta Barbabietola",
+        text: "Uova, farina, acqua, barbabietola",
+        img: "/public/images/pasta_barbabietola.jpeg",
+        tags: ["1", "2", "3"]
+
+    },
+    {
+        title: "Torta Paesana",
+        text: "Uova, farina, lievito, latte...",
+        img: "/public/images/torta_paesana.jpeg",
+        tags: ["1", "2", "3"]
+
+    },
+
+]
+
+module.exports = post;
